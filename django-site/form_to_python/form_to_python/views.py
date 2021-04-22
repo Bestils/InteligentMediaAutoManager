68d783c4f3d83bce3e6f2a9137e9c8c997091a4a
darek <realistic115@wp.pl>
2021-04-22 20:51:22 +0200

    small changes

--- django-site/form_to_python/form_to_python/views.py
+++ django-site/form_to_python/form_to_python/views.py
@@ -1,25 +1,40 @@
 from django.shortcuts import render
 import requests
-
+from django.views.decorators.csrf import csrf_exempt
 from subprocess import run, PIPE
 from django.http import HttpResponse
 import sys
+import time
 from signal import signal, SIGINT
 
 def main(request):
+    context={}
+    if request.method == 'POST':
+        alert = request.POST.get('alert')
+        request.session['alert'] = alert
+        print(request.session['alert'])
+        print(alert)
+        context['alert'] = alert
+        return render(request, 'Login.html', context)
     return render(request, 'Login.html')
 
 def get_data(request):
+    context = {}
     username = request.POST.get('username')
     password = request.POST.get('password')
-    run([sys.executable, '//home//darek//GitHubRepositories//InteligentMediaAutoManager//instagram.py', username, password], shell=False, stdout=PIPE)
-    context = {}
+
+    run([sys.executable, '//home//darek//GitHubRepositories//InteligentMediaAutoManager//django-site//form_to_python//test.py', username, password], shell=False, stdout=PIPE)
     context['login'] = username
     request.session['log'] = username
+    alert = request.session['alert']
+    print(alert)
+    context['alert'] = alert
+    alert = request.session.get('alert')
     return render(request, 'Main.html', context)
 
 def main_page(request):
     login = request.session.get('log')
+    context = {}
     context['login'] = login
     return render(request, 'Main.html', context)
 
