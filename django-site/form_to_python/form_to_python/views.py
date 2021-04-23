9e6cbb36c074e392cf4df5bf94408e081474d3d2
darek <realistic115@wp.pl>
2021-04-23 18:18:26 +0200

    added small form validation for test

--- django-site/form_to_python/form_to_python/views.py
+++ django-site/form_to_python/form_to_python/views.py
@@ -5,48 +5,39 @@ from subprocess import run, PIPE
 from django.http import HttpResponse
 import sys
 import time
-from signal import signal, SIGINT
+context={}
 
 def main(request):
-    context={}
     if request.method == 'POST':
         alert = request.POST.get('alert')
-        request.session['alert'] = alert
-        print(request.session['alert'])
         print(alert)
         context['alert'] = alert
         return render(request, 'Login.html', context)
     return render(request, 'Login.html')
 
 def get_data(request):
-    context = {}
     username = request.POST.get('username')
     password = request.POST.get('password')
 
     run([sys.executable, '//home//darek//GitHubRepositories//InteligentMediaAutoManager//django-site//form_to_python//test.py', username, password], shell=False, stdout=PIPE)
     context['login'] = username
     request.session['log'] = username
-    alert = request.session['alert']
+    alert = context['alert']
     print(alert)
-    context['alert'] = alert
-    alert = request.session.get('alert')
     return render(request, 'Main.html', context)
 
 def main_page(request):
     login = request.session.get('log')
-    context = {}
     context['login'] = login
     return render(request, 'Main.html', context)
 
 def functions(request):
     login = request.session.get('log')
-    context = {}
     context['login'] = login
     return render(request, 'Functions.html', context)
 
 def statistics(request):
     login = request.session.get('log')
-    context = {}
     context['login'] = login
     return render(request, 'Statistics.html', context)
 
