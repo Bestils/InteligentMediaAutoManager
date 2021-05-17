fe68424d0b458c1c5c0d8f2074f569c03f44f6bf
bestils <thebestilspl@gmail.com>
2021-05-17 21:43:04 +0200

    Uprzątnięcie bałagano z folderami

--- django-site/form_to_python/urls.py
+++ django-site/form_to_python/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', csrf_exempt(views.main)),
     path('Main/', csrf_exempt(views.get_data)),
-    path('Start/', views.main_page, name = "Start"),
-    path('Functions/', views.functions, name = "Functions"),
-    path('Statistics/', views.statistics, name = "Statistics"),
-    path('like/', views.like_photos_by_tags, name = "likePhotosByTags")
+    path('Start/', views.main_page, name ="Start"),
+    path('Functions/', views.functions, name ="Functions"),
+    path('Statistics/', views.statistics, name ="Statistics"),
+    path('like/', views.like_photos_by_tags, name ="likePhotosByTags")
 ]
