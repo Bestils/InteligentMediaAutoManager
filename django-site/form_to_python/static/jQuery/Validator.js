ddbda8f117f238da7b3b1394eaf1d922422f7881
darek <realistic115@wp.pl>
2021-06-07 22:09:27 +0200

    still not finished

--- django-site/form_to_python/static/jQuery/Validator.js
+++ django-site/form_to_python/static/jQuery/Validator.js
@@ -1,70 +1,59 @@
-$(document).ready(function(){
-    var possible_tag_chars = "0123456789abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ_ ";
-    var possible_location_chars = "abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ ";
-    var probability_error = "Probability must be a number between 1 and 100";
-    var tag_error = "Tags can only contain letters, numbers, and underscores";
-    var amount_error = "Amount must be a number greater than 0";
-    var delay_error = "Delay must be a number greater than 0";
-    var location_error = "Locations can only contain letters";
-    var green = "rgb(0, 128, 0)";
+var possible_tag_chars = "0123456789abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ_ ";
+var possible_location_chars = "abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ ";
+var probability_error = "Probability must be a number between 1 and 100";
+var tag_error = "Tags can only contain letters, numbers, and underscores";
+var amount_error = "Amount must be a number greater than 0";
+var delay_error = "Delay must be a number greater than 0";
+var location_error = "Locations can only contain letters";
+var green = "rgb(0, 128, 0)";
 
+$(document).ready(function(){
     $("#like_photo_probability_field").focusout(function(){
-            var value = $(this).val()
             probabilityError("like_photo_probability_field", "like_photo_probability_error_field", probability_error);
-            checkOther("like_photo_tags_field", "like_photo_tags_error_field", tag_error, wrongCharsDetector);
+            checkBoth("like_photo_probability_field", "like_photo_probability_error_field", "like_photo_tags_field", "like_photo_tags_error_field");
     });
 
     $("#like_video_probability_field").focusout(function(){
-            var value = $(this).val()
-            checkOther("like_video_tags_field", "like_video_tags_error_field");
             probabilityError("like_video_probability_field", "like_video_probability_error_field", probability_error);
+            checkBoth("like_video_probability_field", "like_video_probability_error_field", "like_video_tags_field", "like_video_tags_error_field");
     });
 
     $("#like_photo_tags_field").focusout(function(){
-        var value = $(this).val();
         wrongCharsDetector("like_photo_tags_field", "like_photo_tags_error_field", tag_error, possible_tag_chars);
-        checkOther("like_photo_probability_field", "like_photo_probability_error_field");
+        checkBoth("like_photo_tags_field", "like_photo_tags_error_field", "like_photo_probability_field", "like_photo_probability_error_field");
     });
 
     $("#like_video_tags_field").focusout(function(){
-        var value = $(this).val();
         wrongCharsDetector("like_video_tags_field", "like_video_tags_error_field", tag_error, possible_tag_chars);
-        checkOther("like_video_probability_field", "like_video_probability_error_field");
+        checkBoth("like_video_tags_field", "like_video_tags_error_field", "like_video_probability_field", "like_video_probability_error_field");
     });
 
     $("#follow_by_tags_field").focusout(function(){
-        var value = $(this).val();
         wrongCharsDetector("follow_by_tags_field", "follow_by_tags_error_field", tag_error, possible_tag_chars);
     });
 
     $("#locations_field").focusout(function(){
-        var value = $(this).val();
         wrongCharsDetector("locations_field", "locations_error_field", location_error, possible_location_chars);
     });
 
     $("#unfollow_non_followers_amount_field").focusout(function(){
-        var value = $(this).val();
         valueError("unfollow_non_followers_amount_field", "unfollow_non_followers_amount_error_field", amount_error)
-        checkOther("unfollow_non_follower_time_field", "unfollow_non_follower_time_error_field");
+        checkBoth("unfollow_non_followers_time_field", "unfollow_non_followers_time_error_field", "unfollow_non_followers_amount_field", "unfollow_non_followers_amount_error_field");
     });
 
     $("#unfollow_non_followers_time_field").focusout(function(){
-        var value = $(this).val();
         valueError("unfollow_non_followers_time_field", "unfollow_non_followers_time_error_field", delay_error);
-        checkOther("unfollow_non_follower_amount_field", "unfollow_non_follower_amount_error_field");
+        checkBoth("unfollow_non_followers_amount_field", "unfollow_non_followers_amount_error_field", "unfollow_non_followers_time_field", "unfollow_non_followers_time_error_field");
     });
 
     $("#unfollow_new_followers_amount_field").focusout(function(){
-        var value = $(this).val();
-        var error = "Amount must be a number greater than 0";
         valueError("unfollow_new_followers_amount_field", "unfollow_new_followers_amount_error_field", amount_error)
-        checkOther("unfollow_new_follower_time_field", "unfollow_new_follower_time_error_field");
+        checkBoth( "unfollow_new_followers_time_field", "unfollow_new_followers_time_error_field", "unfollow_new_followers_amount_field", "unfollow_new_followers_amount_error_field");
     });
 
     $("#unfollow_new_followers_time_field").focusout(function(){
-        var value = $(this).val();
         valueError("unfollow_new_followers_time_field", "unfollow_new_followers_time_error_field", delay_error);
-        checkOther("unfollow_new_follower_amount_field", "unfollow_new_follower_amount_error_field");
+        checkBoth("unfollow_new_followers_amount_field", "unfollow_new_followers_amount_error_field", "unfollow_new_followers_time_field", "unfollow_new_followers_time_error_field");
     });
 
     $("input").focusout(function(){
@@ -77,9 +66,8 @@ function checkForm(green)
     var elements = document.getElementsByTagName("input");
     for(var i = 0; i < elements.length;i++)
     {
-        style = getComputedStyle(elements[i]);
-        console.log(style.borderLeftColor);
-        border = style.borderLeftColor;
+        var style = getComputedStyle(elements[i]);
+        var border = style.borderLeftColor;
 
         if(border == green)
         {
@@ -93,30 +81,31 @@ function checkForm(green)
     }
 }
 
-function valueError(input_field, error_field, error_description)
+function valueError(input, error, error_description)
 {
-    var value = document.getElementById(input_field).value;
+    var value = document.getElementById(input).value;
     if(value > 0)
     {
-        setCorrect(input_field, error_field);
+        setCorrect(input, error);
     }
     else if(value < 0)
     {
-        setError(input_field, error_field, error_description)
+        setError(input, error, error_description);
     }
     else if(value.length == 0)
     {
-        setNeutral(input_field, error_field)
+        setNeutral(input, error);
     }
     else
     {
-        setError(input_field, error_field, error_description)
+        setError(input, error, error_description);
     }
 }
-function wrongCharsDetector(input_field, error_field, error_description, possible_chars)
+
+function wrongCharsDetector(input, error, error_description, possible_chars)
 {
-    var value = document.getElementById(input_field).value;
-    var error = true;
+    var value = document.getElementById(input).value;
+    var wrong = true;
     if(value.length != 0)
     {
         for(var i = 0; i < value.length; i ++)
@@ -125,72 +114,81 @@ function wrongCharsDetector(input_field, error_field, error_description, possibl
             {
                 if(value[i] == possible_chars[j])
                 {
-                    error = false;
+                    wrong = false;
                 }
             }
-            if (error == true)
+            if (wrong == true)
             {
-                setError(input_field, error_field, error_description);
-                return
+                setError(input, error, error_description);
+                return;
             }
         }
-        setCorrect(input_field, error_field);
+        setCorrect(input, error);
     }
-    else{
-        setNeutral(input_field, error_field);
+    else
+    {
+        setNeutral(input, error);
     }
 }
 
-function probabilityError(input_field, error_field, error_description){
-    var value = document.getElementById(input_field).value;
+function probabilityError(input, error, error_description){
+    var value = document.getElementById(input).value;
     if(value.length == 0)
     {
-        setNeutral(input_field, error_field)
+        setNeutral(input, error)
     }
     else if(!isNaN(value) && value > 0 && value <= 100)
     {
-        setCorrect(input_field, error_field);
+        setCorrect(input, error);
     }
     else
     {
-        setError(input_field, error_field, error_description);
+        setError(input, error, error_description);
     }
 }
 
-function setError(input_field, error_field, description)
+function setError(input, error, error_description)
 {
-    document.getElementById(input_field).style.borderColor = "red";
-    document.getElementById(input_field).style.borderWidth = "1px";
-    document.getElementById(error_field).style.color = "red";
-    document.getElementById(error_field).innerHTML = description;
+    document.getElementById(input).style.borderColor = "red";
+    document.getElementById(input).style.borderWidth = "1px";
+    document.getElementById(error).style.color = "red";
+    document.getElementById(error).innerHTML = error_description;
 }
 
-function setCorrect(input_field, error_field)
+function setCorrect(input, error)
 {
-    document.getElementById(input_field).style.borderColor = "green";
-    document.getElementById(input_field).style.borderWidth = "1px";
-    document.getElementById(error_field).innerHTML = "";
+    document.getElementById(input).style.borderColor = "green";
+    document.getElementById(input).style.borderWidth = "1px";
+    document.getElementById(error).innerHTML = "";
 }
 
-function setNeutral(input_field, error_field)
+function setNeutral(input, error)
 {
-    document.getElementById(input_field).style.borderColor = "lightgray";
-    document.getElementById(input_field).style.borderWidth = "0.5px";
-    document.getElementById(error_field).innerHTML = "";
+    document.getElementById(input).style.borderColor = "lightgray";
+    document.getElementById(input).style.borderWidth = "0.5px";
+    document.getElementById(error).innerHTML = "";
 }
 
-function checkOther(input_field, error_field, error, errorCheckMethod)
+function checkBoth(input, input_error, other, other_error)
 {
-    var possible_tag_chars = "0123456789abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ_ ";
-    var value = document.getElementById(input_field).value;
-    console.log(error_field);
-    if(value.length == 0)
+    var value = document.getElementById(input).value;
+    var other_value = document.getElementById(input).value;
+    var input_element = document.getElementById(input);
+    var other_element = document.getElementById(other);
+
+    var other_style = getComputedStyle(other_element);
+    var input_style = getComputedStyle(input_element);
+    if(value.length == 0 && other_style.borderLeftColor == green)
     {
-        setError(input_field, error_field, "This field can't be empty");
+        setError(input, input_error, "This field can't be empty");
+    }
+    else if(input_style.borderLeftColor == green && other_value.length == 0)
+    {
+        setError(other, other_error, "This field can't be empty");
     }
     else
     {
-        errorCheckMethod(input_field, error_field, error, possible_tag_chars);
+        setNeutral(other, other_input);
     }
 }
 
